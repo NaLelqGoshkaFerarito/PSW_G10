7e2957d2dd22c464f816dfde28989e1d14472e33
Vladislav Serafimov <vladiserafimov@abv.bg>
2022-12-06 12:09:55 +0200

    Changed csv log fromat

--- loggers/csv_logger.py
+++ loggers/csv_logger.py
@@ -19,7 +19,7 @@ class CSVLogger(ILogger):
             with open("./loggers/logs_csv/" + self.__file_name, "a") as file:
                 writer = csv.writer(file)
                 # writes the data for device table then for the data table
-                writer.writerow([data.device_id, data.latitude, data.longitude, data.altitude, data.light, data.temperature, data.pressure, data.datetime, datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), data.consumed_airtime])
+                writer.writerow([data.device_id, data.latitude, data.longitude, data.altitude, data.light, data.temperature, data.pressure, data.datetime, datetime.now().strftime("%Y-%m-%d %H:%M:%S"), data.consumed_airtime])
         # else print it for debugging
         else:
             CSVLogger.__console_logger.log(data)
