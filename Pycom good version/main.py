c28e6edac733c2266c66eb5b02a844b0b7e8047d
BogdanI01 <497073@student.saxion.nl>
2023-01-13 09:54:04 +0100

    Update main.py

--- Pycom good version/main.py
+++ Pycom good version/main.py
@@ -26,8 +26,10 @@ from SI7006A20 import SI7006A20
 from LTR329ALS01 import LTR329ALS01
 from MPL3115A2 import MPL3115A2,ALTITUDE,PRESSURE
 ###
-pycom.heartbeat(False)
 
+pycom.heartbeat(False)
+pycom.rgbled(0x007f00) # green
+time.sleep(5)
 
 py = Pycoproc(Pycoproc.PYSENSE)
 
@@ -48,68 +50,73 @@ print('Joined')
 # alt = MPL3115A2(py, mode=ALTITUDE)
     # alt0 = int(alt.altitude())
 
-while(True):
+try:
+        while(True):
+        #Get the pressure data and convert to protocol (pressure = (bytes[0]/2)+950)
+            press = MPL3115A2(py, mode=PRESSURE)
+            press0 = press.pressure() / 100
+            press0 -= 950
+            press0 *= 2
+            press0 = int(press0)
 
-    #Get the pressure data and convert to protocol (pressure = (bytes[0]/2)+950)
-    press = MPL3115A2(py, mode=PRESSURE)
-    press0 = press.pressure() / 100
-    press0 -= 950
-    press0 *= 2
-    press0 = int(press0)
+        # add red light and blue light together.
+            lit = LTR329ALS01(py)
+            lit0 = lit.light()
+            lightCombined = lit0[0] + lit0[1]
 
 
-    # add red light and blue light together.
-    lit = LTR329ALS01(py)
-    lit0 = lit.light()
-    lightCombined = lit0[0] + lit0[1]
+            #temperature = ((bytes[2]-20)*10 + bytes[3])/10
+            temp = SI7006A20(py).temperature()
+            #INTEGER PART (add 20 so that negative values can be send to the network according to protcol)
+            tempInt = int(temp)
+            #DECMIAL PART
+            tempDecimal = int((temp-tempInt) * 100)
 
+            dataArray = [0] * 3
 
-    #temperature = ((bytes[2]-20)*10 + bytes[3])/10
-    temp = SI7006A20(py).temperature()
-    #INTEGER PART (add 20 so that negative values can be send to the network according to protcol)
-    tempInt = int(temp)
-    #DECMIAL PART
-    tempDecimal = int((temp-tempInt) * 100)
+            #print("Altitude: " + str(alt0))
+            print("Pressure: " + str(press0))
+            print("Light: " + str(lit0))
+            print("temp: " + str(temp))
+            print("Temperature: " + str(tempInt) + "." + str(tempDecimal))
 
-    dataArray = [0] * 3
+            if lightCombined > 255:
+                lightCombined = 255
+            elif lightCombined < 0:
+                lightCombined = 0
 
-    #print("Altitude: " + str(alt0))
-    print("Pressure: " + str(press0))
-    print("Light: " + str(lit0))
-    print("temp: " + str(temp))
-    print("Temperature: " + str(tempInt) + "." + str(tempDecimal))
-
-    if lightCombined > 255:
-        lightCombined = 255
-    elif lightCombined < 0:
-        lightCombined = 0
-
-    if press0 > 255:
-        press0 = 255
-    elif press0 < 0:
-        press0 = 0
+            if press0 > 255:
+                press0 = 255
+            elif press0 < 0:
+                press0 = 0
 
     #if temp0 > 255:
     #    temp0 = 255
     #elif temp0 < 0:
     #    temp0 = 0
 
-    dataArray[0] = int(press0)
-    dataArray[1] = int(lightCombined)
-    dataArray[2] = int((lit0[0] + lit0[1])/2)
-    dataArray[2] = int(tempInt) - 10
+            dataArray[0] = int(press0)
+            dataArray[1] = int(lightCombined)
+            dataArray[2] = int((lit0[0] + lit0[1])/2)
+            dataArray[2] = int(tempInt) - 8
 
-    print(dataArray)
+            print(dataArray)
+            s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+            s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 
-    s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-    s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+            s.setblocking(True)
 
-    s.setblocking(True)
+            s.send(bytes([dataArray[0], dataArray[1], dataArray[2]]))
 
-    s.send(bytes([dataArray[0], dataArray[1], dataArray[2]]))
+            time.sleep(300)
 
-    s.setblocking(False)
+            s.setblocking(False)
+except:
+    machine.reset()
+
+
+
+
 
 data = s.recv(128)
 print(data)
-time.sleep(500)
